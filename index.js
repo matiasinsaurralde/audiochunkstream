b10f34768ab779e69a52cc889bfcccdf979428cc
Matias Insaurralde <matias@insaurral.de>
2016-01-03 23:47:42 -0300

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var io = require( 'socket.io' )( 3000 ),
     torrentStream = require( 'torrent-stream')
-
     // memoryStorage = require( 'torrent-memory-storage' )
 
 io.on( 'connection', function( socket ) {
@@ -35,23 +34,6 @@ io.on( 'connection', function( socket ) {
         }
       })
 
-      /*
-      engine.files.forEach( function( file ) {
-        if( file.name.indexOf( request.filename ) > -1 ) {
-          var stream, totalChunks = 0
-          stream.on( 'data', function( chunk ) {
-            var data = { audioData: chunk.toString( 'base64' ) }
-            if( totalChunks == 0 ) {
-              console.log( 'first chunk!' )
-            }
-            socket.emit( 'streamchunk', data )
-            totalChunks++
-          })
-          stream.on( 'end', function() {
-            socket.emit( 'streamend', {} )
-          })
-        }
-      })*/
     })
 
   });
