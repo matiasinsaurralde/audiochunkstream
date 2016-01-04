d18f8091ef7977b8d77ad71b90b6de343acb5be4
Matias Insaurralde <matias@insaurral.de>
2016-01-04 01:42:53 -0300

    debugging chunk

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 angular.module( 'app', [ 'btford.socket-io', 'chunked-audio' ] )
 .factory( 'socket', function( socketFactory ) {
 
-  var socket = socketFactory({ioSocket: io.connect('http://audiochunkstream-931d8e9c.matiasinsaurra7bf05fd94789439a.svc.tutum.io:3000')})
+  // var socket = socketFactory({ioSocket: io.connect('http://audiochunkstream-931d8e9c.matiasinsaurra7bf05fd94789439a.svc.tutum.io:3000')})
+  var socket = socketFactory({ioSocket: io.connect('127.0.0.1:3000') })
 
   socket.streamrequest = function( params ) {
     this.emit( 'streamrequest', params );
@@ -12,6 +13,7 @@ angular.module( 'app', [ 'btford.socket-io', 'chunked-audio' ] )
       console.log( 'receiving chunk!' )
       var shouldForward = filter(e);
       if( shouldForward ) {
+        console.log( 'e', e )
         forwardFunction( e.audioData, e, this );
       };
     });
