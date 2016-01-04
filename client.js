4c94831a1e59ece5863eaaccdb9eb5c6f9daf8d1
Matias Insaurralde <matias@insaurral.de>
2016-01-04 00:59:37 -0300

    pointing to tutum url

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 var io = require( 'socket.io-client' ),
-    socket = io( 'http://127.0.0.1:3000' )
+    socket = io( 'http://audiochunkstream-931d8e9c.matiasinsaurra7bf05fd94789439a.svc.tutum.io:3000' )
 
 socket.on( 'connect', function() {
 
